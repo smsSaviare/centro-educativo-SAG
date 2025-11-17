132c05efcc372b9adaeeb7aaaea2cb6a9e2d5fa8
smsSaviare <saviare2025@gmail.com>
2025-11-17 11:53:54 -0500

    Actualizaciones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-//frontend/src/App.jsx 
+// frontend/src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
 import CourseView from "./components/CourseView";
 import CourseEditor from "./components/CourseEditor";
-import Contacto from "./components/Contacto"; // ✅ correctamente importado
+import Contacto from "./components/Contacto";
 import {
   SignedIn,
   SignedOut,
@@ -78,21 +78,18 @@ function App() {
         {/* 📞 Página de contacto */}
         <Route path="/contacto" element={<Contacto />} />
 
-        {/* 📚 Cursos disponibles o del usuario */}
+        {/* 📚 Cursos */}
         <Route
           path="/courses"
           element={
-            <SignedIn>
-              <Dashboard />
-            </SignedIn>
-          }
-        />
-        <Route
-          path="/courses"
-          element={
-            <SignedOut>
-              <RedirectToSignIn />
-            </SignedOut>
+            <>
+              <SignedIn>
+                <Dashboard />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
           }
         />
 
@@ -100,35 +97,29 @@ function App() {
         <Route
           path="/dashboard"
           element={
-            <SignedIn>
-              <Dashboard />
-            </SignedIn>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <SignedOut>
-              <RedirectToSignIn />
-            </SignedOut>
+            <>
+              <SignedIn>
+                <Dashboard />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
           }
         />
 
-        {/* 📘 Vista del curso */}
+        {/* 📘 Vista curso */}
         <Route
           path="/course/:id"
           element={
-            <SignedIn>
-              <CourseView />
-            </SignedIn>
-          }
-        />
-        <Route
-          path="/course/:id"
-          element={
-            <SignedOut>
-              <RedirectToSignIn />
-            </SignedOut>
+            <>
+              <SignedIn>
+                <CourseView />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
           }
         />
 
@@ -136,23 +127,20 @@ function App() {
         <Route
           path="/editor"
           element={
-            <SignedIn>
-              <RequireRole user={user} role="teacher">
-                <CourseEditor />
-              </RequireRole>
-            </SignedIn>
-          }
-        />
-        <Route
-          path="/editor"
-          element={
-            <SignedOut>
-              <RedirectToSignIn />
-            </SignedOut>
+            <>
+              <SignedIn>
+                <RequireRole user={user} role="teacher">
+                  <CourseEditor />
+                </RequireRole>
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
           }
         />
 
-        {/* 🚪 Cierre de sesión seguro */}
+        {/* 🚪 Logout */}
         <Route
           path="/logout"
           element={
